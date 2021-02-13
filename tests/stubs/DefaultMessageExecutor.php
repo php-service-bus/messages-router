1f614e01a2e310db5f8205701da220d5dfbb53ef
Masiukevich Maksim <contacts@mmasiukevich.dev>
2021-02-13 15:03:59 +0100

    update component

--- tests/stubs/DefaultMessageExecutor.php
+++ tests/stubs/DefaultMessageExecutor.php
@@ -15,13 +15,25 @@ namespace ServiceBus\MessagesRouter\Tests\stubs;
 use Amp\Promise;
 use Amp\Success;
 use ServiceBus\Common\Context\ServiceBusContext;
+use ServiceBus\Common\EntryPoint\Retry\RetryStrategy;
 use ServiceBus\Common\MessageExecutor\MessageExecutor;
+use function ServiceBus\Common\uuid;
 
 /**
  *
  */
 final class DefaultMessageExecutor implements MessageExecutor
 {
+    public function id(): string
+    {
+        return uuid();
+    }
+
+    public function retryStrategy(): ?RetryStrategy
+    {
+        return null;
+    }
+
     public function __invoke(object $message, ServiceBusContext $context): Promise
     {
         return new Success();
