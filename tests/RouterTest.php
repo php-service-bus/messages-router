813e38d57f501f810af1f0bfff27724e21a742b8
mmasiukevich <contacts@desperado.dev>
2021-11-11 10:57:57 +0100

    update php version

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -1,4 +1,6 @@
-<?php /** @noinspection PhpUnhandledExceptionInspection */
+<?php
+
+/** @noinspection PhpUnhandledExceptionInspection */
 
 /**
  * Messages router component.
@@ -8,7 +10,7 @@
  * @license https://opensource.org/licenses/MIT
  */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace ServiceBus\MessagesRouter\Tests;
 
